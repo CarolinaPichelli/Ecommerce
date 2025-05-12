4ff40789f6c03ed940a5fc61eae1fe9a1e7d27c4
Carolina Pichelli <carolinapichellisouza@gmail.com>
2025-05-12 12:35:12 -0300

    test/criacao-teste-unitario

--- EcommerceDDD/ApplicationApp/obj/Debug/netcoreapp3.1/ApplicationApp.AssemblyInfo.cs
+++ EcommerceDDD/ApplicationApp/obj/Debug/netcoreapp3.1/ApplicationApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplicationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f02c306b7078c48487bc117093b536957ed06620")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplicationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplicationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EcommerceDDD/Domain/obj/Debug/netcoreapp3.1/Domain.AssemblyInfo.cs
+++ EcommerceDDD/Domain/obj/Debug/netcoreapp3.1/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f02c306b7078c48487bc117093b536957ed06620")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
